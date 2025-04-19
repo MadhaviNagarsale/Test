e3aefe4bf715c52027a7da6950adc0840448e6a4
Madhavi N <mahdavi.n@domain.com>
2025-04-19 02:14:48 -0700

    commit3

--- jstest.js
+++ jstest.js
@@ -1,2 +1,9 @@
 console.log("Hello World!");
 console.log("this is just an update!!");
+let a="Samosa";
+if (a="Samosa"){
+    console.log("Samosa is ready!")
+}
+else{
+    console.log("other thins is not avaiilable!ss")
+}
